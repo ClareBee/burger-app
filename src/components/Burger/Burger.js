ca094e1abcded613b1b714db1920096326bd4d36
Clare <chblackburne@gmail.com>
2018-03-10 17:21:01 +0000

    Outputs burger ingredients dynamically using state/props/Object.keys/map

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,18 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => {
+const burger = (props) => {
+  //turns object of key-value pairs into an array of ingredients
+  const transformedIngredients = Object.keys(props.ingredients)
+      .map(ingKey => {
+        return [...Array(props.ingredients[ingKey])].map((_, i) => {
+          return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+        });
+      });
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+        {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends React.Component {
+  //modern alternative to constructor
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
   render (){
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     );
