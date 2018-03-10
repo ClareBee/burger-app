6a50ff5c76d3a2b3ca9ce01377b00f20d04bba03
Clare <chblackburne@gmail.com>
2018-03-10 16:54:05 +0000

    Add container for BurgerBuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends React.Component {
+  render (){
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
