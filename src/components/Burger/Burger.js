3de068cf719daf091b2ab54debf64938b1fe2633
Clare <chblackburne@gmail.com>
2018-03-10 17:31:53 +0000

    Flatten array to check length of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
   //turns object of key-value pairs into an array of ingredients
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
       .map(ingKey => {
         return [...Array(props.ingredients[ingKey])].map((_, i) => {
           return <BurgerIngredient key={ingKey + i} type={ingKey} />;
         });
-      });
+      })
+      .reduce((arr, el) => {
+          return arr.concat(el)
+      }, []);
+  if (transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
