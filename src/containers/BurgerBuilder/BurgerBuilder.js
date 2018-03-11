ec6d5b5da92c98ceb871784608cdca501be9674b
Clare <chblackburne@gmail.com>
2018-03-11 12:58:34 +0000

    Adds basic modal set-up

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
 
 // GLOBAL CONSTANTS IN CAPITALS
 const INGREDIENT_PRICES = {
