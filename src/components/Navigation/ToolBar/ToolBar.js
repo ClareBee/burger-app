2121730de8cf8250fd950656814cccda76140ea0
Clare <chblackburne@gmail.com>
2018-03-11 17:18:53 +0000

    Adds navigation bar

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import classes from './ToolBar.css';
 import Logo from '../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={classes.ToolBar}>
     <div>MENU</div>
     <Logo />
-    <nav>...</nav>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
-
 );
 
 export default toolbar;
