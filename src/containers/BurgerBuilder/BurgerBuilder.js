a43b401a14a952564f85c0d558b6ee0d24174c4b
Clare <chblackburne@gmail.com>
2018-03-11 13:23:06 +0000

    Adds backdrop, styling and links to modal visibility

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,6 +85,12 @@ class BurgerBuilder extends React.Component {
     })
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    })
+  }
+
   render (){
     const disabledInfo = {
       ...this.state.ingredients
@@ -94,7 +100,7 @@ class BurgerBuilder extends React.Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
