c55f32bbb9d22ee3e269821e6c89e759800fe9fb
Clare <chblackburne@gmail.com>
2018-03-17 15:14:49 +0000

    Adds mobile first css and media queries for sidedrawer

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -9,7 +9,7 @@ const toolbar = (props) => (
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
