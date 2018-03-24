3bd6adbf90264d5f4faf85b72de7a516366857a6
Clare <chblackburne@gmail.com>
2018-03-24 16:56:46 +0000

    Uses goBack function on react router and sets up next page route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,11 +10,21 @@ class Checkout extends React.Component {
       cheese: 1
     }
   }
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
 
   render () {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients}/>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}/>
       </div>
     );
   }
