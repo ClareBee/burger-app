504bed4d8884ea51eabfc82aed98c4a29229000d
Clare <chblackburne@gmail.com>
2018-03-25 10:21:37 +0100

    Adds NavLink and active styling

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a
-      href={props.link}
-      classes={props.active ? classes.active : null}>{props.children}</a>
+    <NavLink
+      to={props.link}
+      activeClassName={classes.active}>
+      {props.children}
+    </NavLink>
   </li>
 );
 export default navigationItem;
