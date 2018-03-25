945ef8cb3f77ca84becd066ed7e76950cffc152a
Clare <chblackburne@gmail.com>
2018-03-25 10:42:39 +0100

    Outputs orders programmatically

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
-  <div className={classes.Order}>
-    <p>Ingredients:</p>
-    <p>Price <strong></strong></p>
-  </div>
-)
+const order = (props) => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients){
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
+  }
+  const ingredientOutput = ingredients.map(ing => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }}
+        key={ing.name}>{ing.name} ({ing.amount})</span>
+    )
+  })
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+    </div>
+  );
+}
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -28,8 +28,12 @@ class Orders extends React.Component {
   render(){
     return (
       <div>
-        <Order/>
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price} />
+        ))}
       </div>
     );
   }
