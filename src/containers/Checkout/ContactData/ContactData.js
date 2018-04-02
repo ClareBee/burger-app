ab22796dac34f09cb441730dc53d1252355370cf
Clare <chblackburne@gmail.com>
2018-04-02 16:45:48 +0100

    adds customised validation to order form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,11 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
       },
       street: {
         elementType: 'input',
@@ -22,7 +26,11 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
       },
       postCode: {
         elementType: 'input',
@@ -30,7 +38,13 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Postcode'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 7,
+          maxLength: 7,
+        },
+        valid: false,
       },
       country: {
         elementType: 'input',
@@ -38,7 +52,11 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
       },
       email: {
         elementType: 'input',
@@ -46,7 +64,11 @@ class ContactData extends React.Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -57,7 +79,7 @@ class ContactData extends React.Component {
             {value: 'cheapest', displayValue: 'Cheapest'},
           ]
         },
-        value: ''
+        value: '',
       }
     },
     loading: false,
@@ -91,6 +113,21 @@ class ContactData extends React.Component {
         })
       );
   }
+
+  checkValidity(value, rules){
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     //clones - for immutable
     const updatedOrderForm = {
@@ -100,7 +137,9 @@ class ContactData extends React.Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({
       orderForm: updatedOrderForm
     })
