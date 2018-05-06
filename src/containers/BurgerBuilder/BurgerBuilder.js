4ff55cd1a4286ae22e3b192c557f0c9ed0825e9a
Clare <chblackburne@gmail.com>
2018-05-06 14:49:54 +0100

    Calculate price on redux, removes local methods

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,18 +10,9 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import * as actionTypes from '../../store/actions';
 
-// GLOBAL CONSTANTS IN CAPITALS
-const INGREDIENT_PRICES = {
-  salad: 0.5,
-  cheese: 0.4,
-  bacon: 0.7,
-  meat: 1.3
-}
-
 class BurgerBuilder extends React.Component {
   //modern alternative to constructor
   state = {
-    totalPrice: 4,
     purchasable: false,
     purchasing: false,
     loading: false,
@@ -39,43 +30,6 @@ class BurgerBuilder extends React.Component {
     //     error: true
     //   })
     // })
-  }
-
-  addIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type];
-    const updatedCount = oldCount + 1;
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCount;
-    const priceAddition = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceAddition;
-    this.setState({
-      totalPrice: newPrice,
-      ingredients: updatedIngredients
-    })
-    this.updatePurchaseState(updatedIngredients);
-  }
-
-  removeIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type];
-    if (oldCount <= 0){
-      return;
-    }
-    const updatedCount = oldCount - 1;
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCount;
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
-    this.setState({
-      totalPrice: newPrice,
-      ingredients: updatedIngredients
-    });
-    this.updatePurchaseState(updatedIngredients);
   }
 
   updatePurchaseState = (ingredients) => {
@@ -138,7 +92,7 @@ class BurgerBuilder extends React.Component {
             disabled={disabledInfo}
             purchasable={this.state.purchasable}
             ordered={this.purchaseHandler}
-            price={this.state.totalPrice}/>
+            price={this.props.price}/>
           </Aux>
       );
       orderSummary =  (
@@ -146,7 +100,7 @@ class BurgerBuilder extends React.Component {
               purchaseCancelled={this.purchaseCancelHandler}
               purchaseContinued={this.purchaseContinueHandler}
               ingredients={this.props.ings}
-              price={this.state.totalPrice.toFixed(2)}/>
+              price={this.props.price.toFixed(2)}/>
       );
     }
     if(this.state.loading){
@@ -164,7 +118,8 @@ class BurgerBuilder extends React.Component {
 }
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients
+    ings: state.ingredients,
+    price: state.totalPrice
   }
 }
 const mapDispatchToProps = dispatch => {
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,13 @@
 import * as actionTypes from './actions';
 
+// GLOBAL CONSTANTS IN CAPITALS
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  bacon: 0.7,
+  meat: 1.3
+};
+
 const initialState = {
   ingredients: {
     salad: 0,
@@ -19,7 +27,8 @@ const reducer = (state = initialState, action) => {
           //necessary for deep cloning
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        }
+        },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -28,7 +37,8 @@ const reducer = (state = initialState, action) => {
           //necessary for deep cloning
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        }
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       };
     default:
       return state;
