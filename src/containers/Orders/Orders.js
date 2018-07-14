9ef115516d1a90c0c4a0169ba2a35c51d0ba8f31
Clare <chblackburne@gmail.com>
2018-07-14 13:24:35 +0100

    Turns fetching orders into asynch flow with redux and thunk

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,41 +1,44 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends React.Component {
-  state = {
-    orders: [],
-    loading: true
-  }
   componentDidMount(){
-    axios.get('/orders.json')
-      .then(res => {
-        const fetchedOrders = [];
-        // turn object into array
-        for(let key in res.data){
-          fetchedOrders.push({
-            ...res.data[key],
-            id: key
-          });
-        }
-        this.setState({loading: false, orders: fetchedOrders});
-      })
-      .catch( err => {
-        this.setState({loading: false});
-      });
+    this.props.onFetchOrders();
   }
   render(){
+    let orders = <Spinner />
+    if (!this.props.loading){
+      orders =
+        this.props.orders.map(order => (
+        <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          price={order.price} />
+      ))
+    }
     return (
       <div>
-        {this.state.orders.map(order => (
-          <Order
-            key={order.id}
-            ingredients={order.ingredients}
-            price={order.price} />
-        ))}
+        {orders}
       </div>
     );
   }
 }
-export default withErrorHandler(Orders, axios);
+
+const mapStateToProps = state => {
+  return {
+    orders: state.orders.orders,
+    loading: state.orders.loading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchOrders: () => dispatch(actions.fetchOrders())
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
--- src/store/reducers/orders_reducer.js
+++ src/store/reducers/orders_reducer.js
@@ -28,12 +28,28 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: true
-      }
+      };
     case actionTypes.PURCHASE_INIT:
       return {
         ...state,
         purchased: false
-      }
+      };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false
+      };
+    case actionTypes.FETCH_ORDERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+      };
     default:
       return state;
   }
